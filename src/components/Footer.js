fb5065668028fdba4dcb1a4d8a95f38e4ff8affb
Pratik Shastrakar <48235970+pstricks01@users.noreply.github.com>
2022-06-14 20:25:21 +0530

    Homepage created

---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import Logo from "../assets/images/icons8-exercise-64.png";
+
+const Footer = () => (
+	<Box mt="80px" bgcolor="#FFF3F4">
+		<Stack
+			gap="40px"
+			sx={{ alignItems: "center" }}
+			flexWrap="wrap"
+			px="40px"
+			pt="24px"
+		>
+			<img src={Logo} alt="logo" style={{ width: "48px", height: "45px" }} />
+		</Stack>
+		<Typography
+			variant="h5"
+			sx={{ fontSize: { lg: "28px", xs: "20px" } }}
+			mt="41px"
+			textAlign="center"
+			pb="40px"
+		>
+			Made with ❤️ by Pratik Shastrakar
+		</Typography>
+	</Box>
+);
+
+export default Footer;

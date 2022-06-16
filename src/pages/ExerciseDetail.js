9a2064ecfbb8d431ec5b6f87b4e9e33db369abbc
Pratik Shastrakar <48235970+pstricks01@users.noreply.github.com>
2022-06-16 09:44:45 +0530

    Exercises and Equipment added

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -9,6 +9,8 @@ import SimilarExercises from "../components/SimilarExercises";
 const ExerciseDetail = () => {
 	const [exerciseDetail, setExerciseDetail] = useState({});
 	const [exerciseVideos, setExerciseVideos] = useState([]);
+	const [targetMuscleExercises, setTargetMuscleExercises] = useState([]);
+	const [equipmentExercises, setEquipmentExercises] = useState([]);
 	const { id } = useParams();
 
 	useEffect(() => {
@@ -27,6 +29,18 @@ const ExerciseDetail = () => {
 				youtubeOptions
 			);
 			setExerciseVideos(exerciseVideosData.contents);
+
+			const targetMuscleExercisesData = await fetchData(
+				`${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`,
+				exerciseOptions
+			);
+			setTargetMuscleExercises(targetMuscleExercisesData);
+
+			const equimentExercisesData = await fetchData(
+				`${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`,
+				exerciseOptions
+			);
+			setEquipmentExercises(equimentExercisesData);
 		};
 
 		fetchExercisesData();
@@ -39,7 +53,10 @@ const ExerciseDetail = () => {
 				exerciseVideos={exerciseVideos}
 				name={exerciseDetail.name}
 			/>
-			<SimilarExercises />
+			<SimilarExercises
+				targetMuscleExercises={targetMuscleExercises}
+				equipmentExercises={equipmentExercises}
+			/>
 		</Box>
 	);
 };

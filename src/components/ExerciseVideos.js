8991a6a8340838ce3baa51d1d8e5cc30585e984b
Pratik Shastrakar <48235970+pstricks01@users.noreply.github.com>
2022-11-10 10:39:23 +0530

    CICD updated

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -12,7 +12,7 @@ const ExerciseVideos = ({ exerciseVideos, name }) => {
 				<span style={{ color: "#FF2625", textTransform: "capitalize" }}>
 					{name}
 				</span>{" "}
-				exercise videos
+				Exercise videos
 			</Typography>
 			<Stack
 				sx={{ flexDirection: { lg: "row" }, gap: { lg: "110px", xs: "0px" } }}
